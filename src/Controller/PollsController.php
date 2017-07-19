eb18197a9d0b60bc1cbe0e586b4f536dd99bbd72
jendrx <rafael.o.mendonca@gmail.com>
2017-07-19 11:49:09 +0100

    add  buyers, sellers and others top menu bar

--- src/Controller/PollsController.php
+++ src/Controller/PollsController.php
@@ -1,61 +1,61 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: rom
-     * Date: 7/13/17
-     * Time: 11:16 AM
-     */
+/**
+ * Created by PhpStorm.
+ * User: rom
+ * Date: 7/13/17
+ * Time: 11:16 AM
+ */
 
-    namespace App\Controller;
+namespace App\Controller;
 
 
-    class PollsController extends AppController
+class PollsController extends AppController
+{
+    public function initPoll()
     {
-        public function initPoll()
-        {
-            $buyer_id = null;
-            $poll_id = null;
-            if ($this->request->is('ajax')) {
-                $params = $this->request->getQueryParams();
-                $buyer_id = $params['buyer_id'];
-                $poll_id = $this->Polls->init($buyer_id);
-            }
-
-            $this->set(compact('buyer', 'poll_id'));
-            $this->set('_serialize', ['buyer', 'poll_id']);
+        $buyer_id = null;
+        $poll_id = null;
+        if ($this->request->is('ajax')) {
+            $params = $this->request->getQueryParams();
+            $buyer_id = $params['buyer_id'];
+            $poll_id = $this->Polls->init($buyer_id);
         }
 
-        public function getNext()
-        {
-            $poll_id = null;
-            $pair = null;
-            if ($this->request->is('ajax')) {
-                $params = $this->request->getQueryParams();
-                $poll_id = $params['poll_id'];
-                $pair = $this->Polls->getNext($poll_id);
-                $done = false;
+        $this->set(compact('buyer', 'poll_id'));
+        $this->set('_serialize', ['buyer', 'poll_id']);
+    }
 
-                if($pair == null)
-                    $done = true;
-            }
-            $this->set(compact('pair', 'done'));
-            $this->set('_serialize', ['pair','done']);
+    public function getNext()
+    {
+        $poll_id = null;
+        $pair = null;
+        if ($this->request->is('ajax')) {
+            $params = $this->request->getQueryParams();
+            $poll_id = $params['poll_id'];
+            $pair = $this->Polls->getNext($poll_id);
+            $done = false;
 
+            if ($pair == null)
+                $done = true;
         }
-
-        public function vote()
-        {
-            $match_id = null;
-            $winner = null;
-            if ($this->request->is('ajax')) {
-                $params = $this->request->getData();
-                $match_id = $params['match_id'];
-                $winner = $params['winner'];
-                $this->Polls->vote($match_id,$winner);
-
-            }
-            $this->set(compact('match_id', 'winner'));
-            $this->set('_serialize', ['match_id', 'winner']);
-        }
+        $this->set(compact('pair', 'done'));
+        $this->set('_serialize', ['pair', 'done']);
 
     }
+
+    public function vote()
+    {
+        $match_id = null;
+        $winner = null;
+        if ($this->request->is('ajax')) {
+            $params = $this->request->getData();
+            $match_id = $params['match_id'];
+            $winner = $params['winner'];
+            $this->Polls->vote($match_id, $winner);
+
+        }
+        $this->set(compact('match_id', 'winner'));
+        $this->set('_serialize', ['match_id', 'winner']);
+    }
+
+}
